07e710ed43084ee41f9b1dfbf25c6b9a8aa48ae1
Sidney Keese <sidke.z@gmail.com>
2016-07-07 17:29:26 -0700

    moving and spawner components, adding traffic

--- src/javascript/client/index.js
+++ src/javascript/client/index.js
@@ -1,4 +1,46 @@
 import aframe from 'aframe';
-import extras from 'aframe-extras';
 
-extras.registerAll();
+aframe.registerComponent('moving', {
+  schema: {
+    speed: { default: 1 },
+  },
+
+  tick: function() {
+    const direction = new THREE.Vector3(this.data.speed, 0, 0);
+    direction.applyQuaternion(this.el.object3D.quaternion);
+
+    this.el.object3D.position.add(direction);
+
+    if (Math.abs(this.el.object3D.position.x) > 50) {
+      this.el.sceneEl.removeChild(this.el);
+    }
+  }
+});
+
+aframe.registerComponent('spawner', {
+  schema: {
+    timeout: { default: 3000 },
+    mixin: { default: '' },
+  },
+
+  init: function() {
+    this.timer = setInterval(() => this.spawn(), this.data.timeout);
+  },
+
+  spawn: function() {
+    const el = this.el;
+    const scene = el.sceneEl;
+
+    const matrixWorld = el.object3D.matrixWorld;
+    const rotation = el.getAttribute('rotation') || { x: 0, y: 0, z: 0 };
+    const position = new THREE.Vector3;
+    position.setFromMatrixPosition(matrixWorld);
+
+    const entity = document.createElement('a-entity');
+    entity.setAttribute('position', position);
+    entity.setAttribute('mixin', this.data.mixin);
+    entity.setAttribute('rotation', rotation);
+
+    scene.appendChild(entity);
+  }
+});
